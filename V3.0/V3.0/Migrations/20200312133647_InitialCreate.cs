4bc4727c32ccadbafa44c91f2a084e9b73dc7100
Denys Skibinskyi <ruslanskibinskii@gmail.com>
2020-03-12 16:29:18 +0200

    EntityFrameworkCore Testing
    EntityFrameworkCore Testing

---
+++ V3.0/V3.0/Migrations/20200312133647_InitialCreate.cs
@@ -0,0 +1,33 @@
+﻿ using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace V3._0.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cars",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Brand = table.Column<string>(nullable: true),
+                    Price = table.Column<decimal>(nullable: false),
+                    PricePerHour = table.Column<decimal>(nullable: false),
+                    Type = table.Column<string>(nullable: true),
+                    Year = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cars", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cars");
+        }
+    }
+}
---
+++ V3.0/V3.0/Models/Car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace V3._0.Models
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Brand { get; set; }
+        public decimal Price { get; set; }
+        public decimal PricePerHour { get; set; }
+        public string Type { get; set; }
+        public int Year { get; set; }
+    }
+}
